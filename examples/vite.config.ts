3aef82c0773c941f1eb10c9e349b1c2340b01045
Pierre Borckmans <pborckmans@gmail.com>
2024-12-16 01:39:05 +0100

    fix vite config

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -1,16 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'react-konva-interactive-stage': path.resolve(__dirname, '../src/index.ts')
-    }
-  },
   server: {
-    host: true
-  }
-})
+    host: true,
+  },
+});
